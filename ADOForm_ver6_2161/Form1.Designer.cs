2aedddb702f412f19a4887039ec4eeffe025fade
410 <410@DESKTOP-M6RFK49>
2023-11-23 17:38:24 +0900

    초기 커밋.

---
+++ ADOForm_ver6_2161/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace ADOForm_ver6_2161
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.searchB2 = new System.Windows.Forms.Button();
+            this.searchB1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(330, 163);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(375, 144);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // searchB2
+            // 
+            this.searchB2.Location = new System.Drawing.Point(462, 325);
+            this.searchB2.Name = "searchB2";
+            this.searchB2.Size = new System.Drawing.Size(118, 29);
+            this.searchB2.TabIndex = 1;
+            this.searchB2.Text = "비연결 찾기";
+            this.searchB2.UseVisualStyleBackColor = true;
+            this.searchB2.Click += new System.EventHandler(this.searchB2_Click);
+            // 
+            // searchB1
+            // 
+            this.searchB1.Location = new System.Drawing.Point(126, 325);
+            this.searchB1.Name = "searchB1";
+            this.searchB1.Size = new System.Drawing.Size(118, 29);
+            this.searchB1.TabIndex = 2;
+            this.searchB1.Text = "연결 찾기";
+            this.searchB1.UseVisualStyleBackColor = true;
+            this.searchB1.Click += new System.EventHandler(this.searchB1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.White;
+            this.listBox1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(85, 163);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(218, 136);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.UseWaitCursor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "이름";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(93, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 93);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "이름으로 찾기";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.searchB1);
+            this.Controls.Add(this.searchB2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button searchB2;
+        private System.Windows.Forms.Button searchB1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ADOForm_ver6_2161/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types; //***
+
+namespace ADOForm_ver6_2161
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchB1_Click(object sender, EventArgs e)
+        {
+            string ConStr = "User Id=hong1; Password=1111; Data Source=(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME =xe) ) );";
+            OracleConnection conn = new OracleConnection(ConStr);
+            conn.Open();
+            string strqry = "select * from phone where pname =:pname";
+            OracleCommand comm = new OracleCommand(strqry, conn);
+            comm.Parameters.Add("pname", OracleDbType.Varchar2, 20);
+            comm.Parameters["pname"].Value = textBox2.Text.Trim();
+            label2.Text = "select * from phone where pname = '" + textBox2.Text + "'";
+            OracleDataReader sr = comm.ExecuteReader();
+            while (sr.Read())
+            {
+                listBox1.Items.Add(sr["PName"].ToString());
+            }
+            sr.Close();
+            conn.Close();
+
+        }
+
+        private void searchB2_Click(object sender, EventArgs e)
+        {
+            string ConStr = "User Id=hong1; Password=1111; Data Source=(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME =xe) ) );";
+            OracleConnection conn = new OracleConnection(ConStr);
+            conn.Open();
+            OracleDataAdapter DBAdapter = new OracleDataAdapter();
+            DBAdapter.SelectCommand = new OracleCommand
+            ("select * from phone where pname =:pname ", conn);
+            DBAdapter.SelectCommand.Parameters.Add("pname", OracleDbType.Varchar2, 20);
+            DBAdapter.SelectCommand.Parameters["pname"].Value = textBox2.Text.Trim();
+            DataSet DS = new DataSet();
+            DBAdapter.Fill(DS, "Phone");
+            DataTable phoneTable = DS.Tables["Phone"];
+            dataGridView1.DataSource = phoneTable;
+
+        }
+    }
+}
